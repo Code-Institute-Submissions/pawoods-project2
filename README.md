ede8e819fd0685d74cda98f9ac692ea2b2afcb57
pawoods <>
2023-07-02 12:02:40 +0000

    adds event listener to difficulty buttons and gets difficulty from id of each

--- README.md
+++ README.md
@@ -16,7 +16,9 @@
     - [Languages](#languages)
 - [Testing](#testing)
     - [Manual Testing](#manual-testing)
-    - 
+    - [Bugs](#bugs)
+        - [Fixed Bugs](#fixed-bugs)
+        - [Unfixed Bugs](#unfixed-bugs)
 - [Deployment](#deployment)
     - [Live Deployment](#live-deployment)
     - [Local Deployment](#local-deployment)
@@ -50,6 +52,16 @@
 ## Testing
 
 ### Manual Testing
+
+### Bugs
+
+#### Fixed Bugs
+1. Bug found where calling any show**Screen function would hide the contact button in the header.
+- Fixed by changing the class name from "contact" to "contact-button" as the contact screen had the class "contact".
+---
+
+
+#### Unfixed Bugs
 
 ## Deployment
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,13 @@ const difficultyButtons = document.querySelectorAll(".difficulty-buttons .button
 // Event Listeners
 homeButtons.forEach((button) => button.addEventListener("click", showHomeScreen));
 contactButtons.forEach((button) => button.addEventListener("click", showContactScreen));
-
+difficultyButtons.forEach((button) => button.addEventListener("click", function(e) {
+    let difficulty = e.target.id;
+    createGame(difficulty);
+}));
 
 // Functions
+// Show only home screen
 function showHomeScreen() {
     startScreen.classList.remove("hidden");
     gameScreen.classList.add("hidden");
@@ -23,7 +27,7 @@ function showHomeScreen() {
     contactScreen.classList.add("hidden");
     console.log("HOME!");
 }
-
+// Show only game screen
 function showGameScreen() {
     startScreen.classList.add("hidden");
     gameScreen.classList.remove("hidden");
@@ -31,7 +35,7 @@ function showGameScreen() {
     contactScreen.classList.add("hidden");
     console.log("GAME!");
 }
-
+// Show only end screen
 function showEndScreen() {
     startScreen.classList.add("hidden");
     gameScreen.classList.add("hidden");
@@ -39,11 +43,15 @@ function showEndScreen() {
     contactScreen.classList.add("hidden");
     console.log("END!");
 }
-
+// Show only contact screen
 function showContactScreen() {
     startScreen.classList.add("hidden");
     gameScreen.classList.add("hidden");
     endScreen.classList.add("hidden");
     contactScreen.classList.remove("hidden");
     console.log("CONTACT!");
+}
+// Create game from chosen difficulty
+function createGame(difficulty) {
+    console.log(difficulty);
 }
